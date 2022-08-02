ec0ffe04122513c9f78ae331139f555efb45f767
GiacomoPasqualiMiller <giacomo.pasquali@millergroup.it>
2022-08-02 11:06:48 +0200

    Auto Effect

--- js/carcater-effects.js
+++ js/carcater-effects.js
@@ -2,8 +2,8 @@ const imgIronMan = document.getElementById('iron')
 const imgWanda = document.getElementById('wanda')
 const imgBlack = document.getElementById('black-panter')
 const imgCap = document.getElementById('capitan-marvel')
+const imgRegoleFirst = document.getElementById('first-card')
 const popUp = document.getElementById('popup-modal')
-
 // Get the modal by id
 var modal = document.getElementById("modal");
 
